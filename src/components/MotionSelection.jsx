a058bd64359fedac6fa715b78bd140d656845f04
Mysticman22 <mysticeldrago@gmail.com>
2026-01-14 15:09:24 +0530

    motion selection

---
+++ src/components/MotionSelection.jsx
@@ -0,0 +1,20 @@
+import { motion } from "framer-motion";
+import { useCalmScroll } from "../hooks/useCalmScroll";
+
+function MotionSection({ children }) {
+  const { y, opacity } = useCalmScroll();
+
+  return (
+    <motion.div
+      style={{ y, opacity }}
+      transition={{
+        duration: 1.4,
+        ease: [0.22, 1, 0.36, 1],
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+export default MotionSection;
