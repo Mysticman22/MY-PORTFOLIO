198e262e2307a809acaa437219945eabb689fe87
Mysticman22 <mysticeldrago@gmail.com>
2026-01-25 15:07:06 +0530

    tech skills to add

---
+++ src/data/techStack.js
@@ -0,0 +1,33 @@
+export const techStack = {
+  "Frontend Designing": [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "Tailwind CSS",
+  ],
+  "Backend Development": [
+    "Node.js",
+    "Express",
+    "REST APIs",
+    "Authentication",
+  ],
+  "Programming Languages": [
+    "Java",
+    "Python",
+    "JavaScript",
+  ],
+  "Tools & Platforms": [
+    "Git & GitHub",
+    "Docker",
+    "Google Cloud",
+    "VS Code",
+  ],
+  "CS Fundamentals": [
+    "Data Structures & Algorithms",
+    "OOPs",
+    "Operating Systems",
+    "DBMS",
+    "Computer Networks",
+  ],
+};
